5b00e99366f053c9d8988ae6108f6c90f238810a
Tongyu Wu <1419911360@qq.com>
2024-12-01 20:07:07 +0000

    add plane

--- game.cpp
+++ game.cpp
@@ -12,33 +12,36 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 {
 	Window win;
 	DXcore core;
-	Shader shader, shader_static;
+	Shader shader_animated, shader_static;
 	Model_static tree;
-	Model_animated model;
+	Plane plane;
+	Model_animated trex;
 	AnimationInstance instance;
 
 	GamesEngineeringBase::Timer timer;
 	float WIDTH = 1024.f;
 	float HEIGHT = 1024.f;
-	Matrix worldMatrix1, worldMatrix2, worldMatrix_tree, worldMatrix_trex;
+	Matrix worldMatrix_plane, worldMatrix_tree, worldMatrix_trex;
 	Matrix view, projection, vp;
 	Vec3 from;
 
 	win.create(1024, 1024, "My Window");
 	core.init(1024, 1024, win.hwnd, false);
-	shader.init("VertexShader_anim.txt","PixelShader_anim.txt",&core);
+	shader_animated.init("VertexShader_anim.txt","PixelShader_anim.txt",&core);
 	shader_static.init("VertexShader_1125.txt", "PixelShader_1125.txt", &core);
 	tree.init("acacia_003.gem", &core);
-	model.init("TRex.gem", &core);
+	plane.init(&core);
+	trex.init("TRex.gem", &core);
 	float time = 0.f;
 	float fov = 45.0f;
 
-	worldMatrix_tree = worldMatrix_tree.worldMatrix(Vec3(0, 0, 0), Vec3(0.01, 0.01, 0.01), 0, 0, 0);
-	worldMatrix_trex = worldMatrix_tree.worldMatrix(Vec3(0, 0, 0), Vec3(1, 1, 1), 0, 0, 0);
+	worldMatrix_tree = Matrix::worldMatrix(Vec3(-4, 0, -4), Vec3(0.03, 0.03, 0.03), 0, 0, 0);
+	worldMatrix_trex = Matrix::worldMatrix(Vec3(0, 0, 0), Vec3(1, 1, 1), 0, 0, 0);
+	worldMatrix_plane = Matrix::worldMatrix(Vec3(0, 0, 0), Vec3(1, 1, 1), 0, 0, 0);
 	projection.perspectiveProjection(1024.f / 1024.f, tan(fov * 5 * M_PI / 180));
 
 	//initialize animation instance
-	instance.animation = &model.animation;  
+	instance.animation = &trex.animation;  
 	instance.currentAnimation = "Run";      
 
 	while (true) {
@@ -49,24 +52,31 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 
 		instance.update("Run", dt);
 
-
 		from = Vec3(20 * cos(time), 10, 20 * sinf(time));
-		//view = view.lookAtMat(Vec3(0,1,1), Vec3(0, 1, 0), Vec3(0, 1, 0));
 		view = view.lookAtMat(from, Vec3(0, 1, 0), Vec3(0, 1, 0));
 		vp =  view * projection;
 
+		// draw plane
+		shader_static.updateConstantVS("StaticModel", "staticMeshBuffer", "W", &worldMatrix_plane);
+		shader_static.updateConstantVS("StaticModel", "staticMeshBuffer", "VP", &vp);
+
+		shader_static.apply(&core);
+		plane.mesh.draw(&core);
+		
+		// draw tree
 		shader_static.updateConstantVS("StaticModel", "staticMeshBuffer", "W", &worldMatrix_tree);
 		shader_static.updateConstantVS("StaticModel", "staticMeshBuffer", "VP", &vp);
 
 		shader_static.apply(&core);
 		tree.draw(&core);
 		
-		shader.updateConstantVS("Animated", "staticMeshBuffer", "bones", instance.matrices);
-		shader.updateConstantVS("Animated", "staticMeshBuffer", "W", &worldMatrix_trex);
-		shader.updateConstantVS("Animated", "staticMeshBuffer", "VP", &vp);
+		// draw trex
+		shader_animated.updateConstantVS("Animated", "staticMeshBuffer", "bones", instance.matrices);
+		shader_animated.updateConstantVS("Animated", "staticMeshBuffer", "W", &worldMatrix_trex);
+		shader_animated.updateConstantVS("Animated", "staticMeshBuffer", "VP", &vp);
 
-		shader.apply(&core);
-		model.draw(&core);
+		shader_animated.apply(&core);
+		trex.draw(&core);
 
 		core.present();
 
